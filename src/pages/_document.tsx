40c8cc47b45d920e233d04d58096e0b3fe6a15d4
Catalina Fernandez <catalina.fernandez@m6connect.com>
2023-03-18 13:39:10 -0300

    wip

---
+++ src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body className='vw-100 vh-100'>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
